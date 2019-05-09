ba4502c7b38218ec7bb12dab7d7abfb648e04d15
Yodel-oh-ee-dee <42037023+Cheg-ya@users.noreply.github.com>
2019-05-09 21:07:34 +0900

    set up babel & react

--- src/index.js
+++ src/index.js
@@ -7,7 +7,6 @@ function component() {
   const btn = document.createElement('button');
   // Lodash, currently included via a script, is required for this line to work
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-
   element.appendChild(btn);
 
   btn.innerHTML = 'click me';
